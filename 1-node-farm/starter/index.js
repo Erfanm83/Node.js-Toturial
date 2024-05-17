cf06d1ddc8cba0b11f9d385eb53e3905e7799fab
erfan <erfanmahmudi1383@gmail.com>
2024-05-18 03:18:56 +0330

    update files

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -32,6 +32,24 @@ const url = require('url');
 
 //////////////////////////////////////////////
 // Server
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCT_NAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+    
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+}
+
+const tempOverveiw = fs.readFileSync(`${__dirname}/templates/template-overview.html` , 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html` , 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html` , 'utf-8');
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json` , 'utf-8');
 const DataObj = JSON.parse(data);
 
@@ -39,8 +57,13 @@ const server = http.createServer((req , res) => {
     const pathname = req.url;
 
     // Overveiw Page
-    if (pathname === '/' || pathname === '/overview')
-        res.end('This is the Overview');
+    if (pathname === '/' || pathname === '/overview') {
+        res.writeHead(200 , {'Content-type' : 'text/html'});
+
+        const cardsHtml = DataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverveiw.replace('{%PRODUCT_CARDS%}' , cardsHtml);
+        res.end(output);
+    }
 
     // Product Page
     else if (pathname === '/product')
